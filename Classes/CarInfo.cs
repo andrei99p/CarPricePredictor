a4b9299385b80dff1c527d34fa3182dd5a5bf027
Dragos-Andrei Popescu <andrei.popescu@infofer.ro>
2023-10-31 16:05:27 +0200

    Add project files.

---
+++ Classes/CarInfo.cs
@@ -0,0 +1,29 @@
+﻿namespace CarPricePredictor.Classes
+{
+    public class CarInfo
+    {
+        public string? Brand { get; set; }
+        public string? FuelType { get; set; }
+        public string? Aspiration { get; set; }
+        public string? Doors { get; set; }
+        public string? BodyStyle { get; set; }
+        public string? DriveWheels { get; set; }
+        public string? EngineLocation { get; set; }
+        public string? WheelBase { get; set; }
+        public string? Length { get; set; }
+        public string? Width { get; set; }
+        public string? Height { get; set; }
+        public string? CurbWeight { get; set; }
+        public string? EngineType { get; set; }
+        public string? NoOfCylinders { get; set; }
+        public string? EngineSize { get; set; }
+        public string? FuelSystem { get; set; }
+        public string? Bore { get; set; }
+        public string? Stroke { get; set; }
+        public string? CompressionRatio { get; set; }
+        public string? Horsepower { get; set; }
+        public string? PeakRpm { get; set; }
+        public string? CityMpg { get; set; }
+        public string? HighwayMpg { get; set; }
+    }
+}
---
+++ Classes/Client.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CarPricePredictor.Classes
+{
+
+    public class Client: ComponentBase
+    {
+        private HttpClient _httpClient;
+
+        public Client(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        private async Task CallML()
+        {
+
+        }
+    }
+}
